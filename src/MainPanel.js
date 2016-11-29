0f487bbf6f30ba163be55cfd8b69e87f8b27089c
Steven McAdams <smcadams86@gmail.com>
2016-11-29 08:40:57 -0600

    remove noisy console log

--- src/MainPanel.js
+++ src/MainPanel.js
@@ -74,7 +74,6 @@ class MainPanel extends React.Component {
     }
 
     if (this.state.loading === true) {
-      console.log('Still loading sermons...');
       return <ul>Loading...</ul>;
     }
     else {
