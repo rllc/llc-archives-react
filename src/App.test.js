7a1b69adaf2d20391f48c217db5a2054a0662af1
Steven McAdams <smcadams86@gmail.com>
2016-12-08 17:05:50 -0600

    fix broken test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 import App from './App';
+import './index.css';
+
+window.matchMedia = window.matchMedia || function() {
+    return {
+        matches : false,
+        addListener : function() {},
+        removeListener: function() {}
+    };
+};
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
