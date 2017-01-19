68faffcc8b2cdecb0d5967af00f792ff95682dc7
Steve <smcadams86@gmail.com>
2017-01-19 00:36:07 -0600

    fix broken time-specific test

--- src/services/DateFormattingService.test.js
+++ src/services/DateFormattingService.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import DateFormattingService from './DateFormattingService';
 
 it('formats dates in long format', () => {
-  const date = new Date(Date.UTC(2012, 11, 20, 0, 0, 0));
+  const date = new Date(Date.UTC(2012, 11, 20, 12, 0, 0));
   const formattedDate = DateFormattingService.formatDate(date);
 
-  expect (formattedDate).toBe('Wednesday, December 19, 2012');
+  expect (formattedDate).toBe('Thursday, December 20, 2012');
 });
